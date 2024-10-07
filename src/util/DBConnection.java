39c32dce9a0280870a141150141ee9498af6de84
aansh06 <aansh0611@gmail.com>
2024-10-07 17:13:40 +0530

    final upload

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -3,19 +3,38 @@ package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
     private static Connection connection = null;
 
     public static Connection getConnection() {
+//        if (connection == null) {
+//            try {
+//                Class.forName("");
+//                connection = DriverManager.getConnection("", "", "");
+//            } catch (ClassNotFoundException | SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        return connection;
+
         if (connection == null) {
             try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/crimeAnalysisReportingSystem", "root", "rootuser");
-            } catch (ClassNotFoundException | SQLException e) {
+                Properties properties = DBPropertyUtil.loadProperties();
+                String url = properties.getProperty("db.url");
+                String username = properties.getProperty("db.username");
+                String password = properties.getProperty("db.password");
+                String driver = properties.getProperty("db.driver");
+
+                Class.forName(driver);
+
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
         return connection;
+
     }
 }
