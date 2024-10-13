1652515728aa87ff41266a372b55a3b817b8b278
aansh06 <aansh0611@gmail.com>
2024-10-13 21:19:09 +0530

    file

---
+++ src/exception/VictimNotCreatedException.java
@@ -0,0 +1,19 @@
+package exception;
+
+public class VictimNotCreatedException extends Exception {
+    public VictimNotCreatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+public class SuspectNotCreatedException extends Exception {
+    public SuspectNotCreatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+public class IncidentNotCreatedException extends Exception {
+    public IncidentNotCreatedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
